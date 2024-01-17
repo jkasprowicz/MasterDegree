245f9c09b83c9a8ad8bde53b8fcd86387b9a47d1
jkasprowicz <joao.kasprowicz@univali.br>
2024-01-17 20:41:55 -0300

    working_on_upload_image

--- MestradoExample/MestradoExample/urls.py
+++ MestradoExample/MestradoExample/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import home_view, upload_image
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +26,6 @@ urlpatterns = [
     path('upload/', upload_image, name='upload_image'),
 
 ]
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- MestradoExample/MestradoExample/utils.py
+++ MestradoExample/MestradoExample/utils.py
@@ -4,7 +4,9 @@ from detectron2.engine import DefaultPredictor
 
 def load_detectron2_model(model_path, config_path):
     cfg = get_cfg()
-    cfg.merge_from_file(model_zoo.get_config_file(config_path))
+    cfg.set_new_allowed(True)  
+    cfg.merge_from_file(config_path)  # Use your custom config file directly
+    cfg.MODEL.DEVICE = 'cpu' 
     cfg.MODEL.WEIGHTS = model_path
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # Adjust threshold as needed
     predictor = DefaultPredictor(cfg)
