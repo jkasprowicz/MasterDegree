883a452f8952512bb4fc24dada30afc803552d19
jkasprowicz <joao.kasprowicz@univali.br>
2024-01-17 18:58:01 -0300

    models_and_utils

--- MestradoExample/MestradoExample/utils.py
+++ MestradoExample/MestradoExample/utils.py
@@ -1,18 +1,11 @@
-# utils.py
 from detectron2 import model_zoo
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultPredictor
-import cv2
 
-def perform_inference(image_path):
-    # Your Detectron2 inference logic here
-    # ...
-
-    # Dummy result for illustration
-    inference_results = {
-        'count': 42,
-        'bounding_boxes': [(x1, y1, x2, y2), ...],
-        # Add other relevant information
-    }
-
-    return inference_results
+def load_detectron2_model(model_path, config_path):
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file(config_path))
+    cfg.MODEL.WEIGHTS = model_path
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # Adjust threshold as needed
+    predictor = DefaultPredictor(cfg)
+    return predictor
