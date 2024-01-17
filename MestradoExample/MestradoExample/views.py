b6a4c116434da4e0f94a0d12b54ca6ba4b68444f
jkasprowicz <joao.kasprowicz@univali.br>
2024-01-17 19:02:39 -0300

    views_home_upload_image

--- MestradoExample/MestradoExample/views.py
+++ MestradoExample/MestradoExample/views.py
@@ -2,26 +2,57 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib import messages
-# views.py
 from django.shortcuts import render, redirect
 from .forms import ImageUploadForm
 from .models import UploadedImage
-from .utils import perform_inference
+from .utils import load_detectron2_model
 from django.http import JsonResponse
+import cv2
+import json
 
 def home_view(request):
     return render(request, 'home.html')
 
 
 def upload_image(request):
+    print("Reached upload_image view")
     if request.method == 'POST':
         form = ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            image_instance = form.save()
-            inference_results = perform_inference(image_instance.image.path)
-            return JsonResponse({'report': inference_results})
-    else:
+            plate_name = form.cleaned_data['plate_name']
+            image_instance = form.save(commit=False)
+            image_instance.plate_name = plate_name  # Add plate_name to the image instance
+            image_instance.save()
+
+            model_path = '/Users/joaokasprowicz/MasterDegree/MestradoExample/models/model_final.pth'
+            config_path = '/Users/joaokasprowicz/MasterDegree/MestradoExample/models/config.yaml'
+            predictor = load_detectron2_model(model_path, config_path)
+
+            # Load model and perform inference
+            image_path = image_instance.image.path
+            image = cv2.imread(image_path)
+            outputs = predictor(image)
+
+                        # Extract relevant information from the Instances object
+            pred_boxes = outputs["instances"].pred_boxes.tensor.tolist()
+            scores = outputs["instances"].scores.tolist()
+            pred_classes = outputs["instances"].pred_classes.tolist()
+
+            # Convert the information to a serializable format
+            results = {
+                'pred_boxes': pred_boxes,
+                'scores': scores,
+                'pred_classes': pred_classes,
+            }
+
+            print(outputs)            
+
+            print(json.dumps({'report': results}))
+
+
+            # Return the results as JSON
+            return JsonResponse({'report': json.dumps(results)})
+        
         form = ImageUploadForm()
 
     return render(request, 'upload_template.html', {'form': form})
-
