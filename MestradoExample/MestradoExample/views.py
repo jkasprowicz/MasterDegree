f6c459bf0e2e85751c6879f49b71ab398cb1d9a6
jkasprowicz <joao.kasprowicz@univali.br>
2024-01-18 15:03:45 -0300

    multiples_images_and_report

--- MestradoExample/MestradoExample/views.py
+++ MestradoExample/MestradoExample/views.py
@@ -22,38 +22,48 @@ def upload_image(request):
         form = ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
             plate_name = form.cleaned_data['plate_name']
-            image_instance = form.save(commit=False)
-            image_instance.plate_name = plate_name  # Add plate_name to the image instance
-            image_instance.save()
+            images = request.FILES.getlist('image')  # Handle multiple images
+
+            results_list = []  # To store results for each image
 
             model_path = '/Users/joaokasprowicz/MasterDegree/MestradoExample/models/model_final.pth'
             config_path = '/Users/joaokasprowicz/MasterDegree/MestradoExample/models/config.yaml'
             predictor = load_detectron2_model(model_path, config_path)
 
-            # Load model and perform inference
-            image_path = image_instance.image.path
-            image = cv2.imread(image_path)
-            outputs = predictor(image)
+            for image_file in images:
+                image_instance = UploadedImage(image=image_file, plate_name=plate_name)
+                image_instance.save()
 
-                        # Extract relevant information from the Instances object
-            pred_boxes = outputs["instances"].pred_boxes.tensor.tolist()
-            scores = outputs["instances"].scores.tolist()
-            pred_classes = outputs["instances"].pred_classes.tolist()
+                # Load model and perform inference
+                image_path = image_instance.image.path
+                image = cv2.imread(image_path)
+                outputs = predictor(image)
 
+                # Extract relevant information from the Instances object
+                pred_boxes = outputs["instances"].pred_boxes.tensor.tolist()
+                scores = outputs["instances"].scores.tolist()
+                pred_classes = outputs["instances"].pred_classes.tolist()
 
-            image_relative_path = os.path.relpath(image_path, settings.MEDIA_ROOT)
+                image_relative_path = os.path.relpath(image_path, settings.MEDIA_ROOT)
 
+                # Convert the information to a serializable format
+                results = {
+                    'pred_boxes': pred_boxes,
+                    'scores': scores,
+                    'pred_classes': pred_classes,
+                    'image_path': image_relative_path,
+                }
 
-            # Convert the information to a serializable format
-            results = {
-                'pred_boxes': pred_boxes,
-                'scores': scores,
-                'pred_classes': pred_classes,
-                'image_path': image_relative_path            
-            }
+                results_list.append(results)
 
-            return JsonResponse({'report': results})
+                print(results)
+
+            
+            return JsonResponse({'report': results_list})
+
         
         form = ImageUploadForm()
 
     return render(request, 'upload_template.html', {'form': form})
+
+
